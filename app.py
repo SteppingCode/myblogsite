6e4234927ad6dfe268fdda3864fa220ab029ce92
mrice <mricekanal@gmail.com>
2023-05-08 11:51:02 +0300

    Небольшие корректировки в дизайне

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def first_page():
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/myblogweb/myblogsite')
+        repo = git.Repo('dodik337/myblogsite')
         origin = repo.remotes.origin
         origin.pull()
         return 'Сайт обновился', 200
